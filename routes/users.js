1c840dc7a6d6405c9b0f52213639ec757aee5bc4
tadeogavensky <tadeogavensky.email@gmail.com>
2022-04-25 14:14:38 -0300

    OT176-40-servicio-reutilizable-para-subida-de-imagenes

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,13 @@ var router = express.Router();
 const userController = require('../controllers/userController')
 const userValidation = require('../validations/user.js')
 
+const upload = require('../utils/multer')
+
+
 router.post('/auth/signup', userValidation.signup, userController.signup)
 router.post('/auth/login', userValidation.login, userController.login)
+router.post('/auth/upload', upload, userController.awsImageUploader)
+
 
 
 module.exports = router;
