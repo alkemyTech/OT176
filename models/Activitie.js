5a094a6d4bad8216de5b5146e3f86ec66fc74a4a
NachooLopez <ignacio.fco.lopez@gmail.com>
2022-04-25 18:55:03 -0300

    changes model name

--- models/Activitie.js
+++ models/Activitie.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Activity.init(
+  activitie.init(
     {
       name: {
         type: DataTypes.STRING,
