ef1c6ab9ac0e4dda0e66b2b1f2d390692fed39ee
Fede Cione <fede_cione_96@hotmail.com>
2022-04-26 00:23:00 -0300

    Adding user controller base

---
+++ controllers/userController.js
@@ -0,0 +1,20 @@
+const db = require("../models");
+
+const userController = {
+  // Start User CRUD
+  userList: (req, res, next) => {
+
+  },
+  userEdit: (req, res, next) => {
+
+  },
+  signup: (req, res, next) => {
+
+  },
+  userDelete: (req, res, next) => {
+
+  },
+  // End User CRUD
+};
+
+module.exports = userController;
