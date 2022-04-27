58f81864d23004844d490bc5e827fa044a3a6f64
tadeogavensky <tadeogavensky.email@gmail.com>
2022-04-27 20:34:38 -0300

    validaciones agregadas a la subida de imagenes

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,17 @@
 var express = require('express')
 var router = express.Router()
 
-const authAdmin = require('../middlewares/authAdmin')
-const {
-  userList,
-  signup,
-  login,
-  userEdit
-} = require('../controllers/userController')
+const authAdmin = require('../middlewares/authAdmin');
+const { userList, signup, login, userEdit } = require('../controllers/userController');
 const userValidation = require('../validations/user.js')
 const upload = require('../utils/multer')
 const awsImageUploader = require('../utils/awsImageUploader')
+const imageValidator = require('../validations/image')
 
-/* GET users listing. */
 router.get('/users', authAdmin, userList)
 router.post('/users/:id', userEdit)
-router.post('/auth/signup', userValidation.signup, signup)
-router.post('/auth/login', userValidation.login, login)
-router.post('/auth/upload', upload, authAdmin, awsImageUploader)
+router.post('/signup', userValidation.signup, signup)
+router.post('/login', userValidation.login, login)
+router.post('/auth/awsImgUpload', authAdmin, upload, imageValidator, awsImageUploader)
 
 module.exports = router
--- utils/awsImageUploader.js
+++ utils/awsImageUploader.js
@@ -4,46 +4,54 @@ const s3 = require('../utils/s3')
 const {
     v4: uuidv4
 } = require('uuid');
-
+const {
+    validationResult
+} = require("express-validator");
 
 const uploadImg = async (req, res) => {
 
     try {
-        let myFile = await req.file.originalname.split(".")
-        console.log('myFile', myFile)
-        const fileType = await myFile[myFile.length - 1]
-    
-        console.log('myFile', myFile)
-        console.log('fileType', fileType)
-    
-        const params = {
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: `${uuidv4()}.${fileType}`,
-            Body: req.file.buffer,
-            ACL: 'public-read',
-            ContentEncoding: 'base64',
-            ContentDisposition: 'inline',
-            ContentType: 'image/jpeg',
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+            });
+        } else {
+            let myFile = await req.file.originalname.split(".")
+            console.log('myFile', myFile)
+            const fileType = await myFile[myFile.length - 1]
+
+            console.log('myFile', myFile)
+            console.log('fileType', fileType)
+
+            const params = {
+                Bucket: process.env.AWS_BUCKET_NAME,
+                Key: `${uuidv4()}.${fileType}`,
+                Body: req.file.buffer,
+                ACL: 'public-read',
+                ContentEncoding: 'base64',
+                ContentDisposition: 'inline',
+                ContentType: 'image/jpeg',
+            }
+
+
+            s3.upload(params, (error, data) => {
+                if (error) {
+                    res.status(500).send(error)
+                }
+                let response = {
+                    location: 'https://s3.console.aws.amazon.com/s3/buckets/ong-bucket-alkemy?region=us-east-1&tab=objects',
+                    image: `https://${params.Bucket}.s3.amazonaws.com/${data.Key}`,
+                    bucket: data.Bucket,
+                }
+                res.json(response)
+            })
         }
-    
-        //Validación de si el usuario es adminsitrador, si lo es ejecuta el codigo de abajo
-        console.log('params', params)
-       s3.upload(params, (error, data) => {
-            if (error) {
-                res.status(500).send(error)
-            }
-            let response = {
-                location: 'https://s3.console.aws.amazon.com/s3/buckets/ong-bucket-alkemy?region=us-east-1&tab=objects',
-                image: `https://${params.Bucket}.s3.amazonaws.com/${data.Key}`,
-                bucket: data.Bucket,
-            }
-            res.json(response)
-        })
     } catch (error) {
         console.log('error', error)
     }
 
-   
+
 }
 
 
