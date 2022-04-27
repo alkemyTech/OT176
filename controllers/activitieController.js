756eebba522e88d96ec764bc245064362505b651
Ignacio Lopez <92762013+NachooLopez@users.noreply.github.com>
2022-04-27 13:14:29 -0300

    OT176-52-Endpoint Creación Actividades (#22)

--- controllers/activitieController.js
+++ controllers/activitieController.js
@@ -1,9 +1,18 @@
-const db = require('../models')
+const { Activity } = require("../models/index");
 
-const activitieController = {
+module.exports = {
+  postActivities: async (req, res, next) => {
+    try {
+      const { name, image, content } = req.body;
 
-}
+      const activity = await Activity.create({ name, image, content });
 
-
-
-module.exports = activitieController;
+      res.status(201).json({
+        message: "Activity created successfully",
+        activity,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
