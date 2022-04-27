dd6a90d4e2bd63c85e8fac8c1a6ecd44df5827eb
Ignacio Lopez <92762013+NachooLopez@users.noreply.github.com>
2022-04-27 13:15:19 -0300

    Ot176 49 Crear migración y modelo de Actividades (#21)
    * OT176-49-Crear migración y modelo de Actividades
    
    * minor fix
    
    * changes model name

--- models/Activitie.js
+++ models/Activitie.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class activitie extends Model {
     /**
@@ -12,15 +10,31 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  activitie.init({
-    name: DataTypes.STRING,
-    content: DataTypes.TEXT,
-    image: DataTypes.STRING,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'activitie',
-  });
-  return activitie;
+  }
+  activitie.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isUrl: true,
+        },
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Activity",
+      paranoid: true,
+    }
+  );
+  return Activity;
 };
