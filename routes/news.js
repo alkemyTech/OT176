08426eed160005cb0b7d4663627111ef2153a159
maxiconstancio <maxiconstancio@gmail.com>
2022-04-29 18:34:40 -0300

    add route delete news

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
 const router = express.Router();
+const validate = require('../middlewares/validate');
+const newValidator = require('../validations/news');
 const newController = require('../controllers/newController');
+const authAdmin = require('../middlewares/authAdmin');
+const authenticated = require('../middlewares/authenticated');
 
 /* GET */
 router.get('/', newController.list);
+router.get('/:id', authenticated, authAdmin, newController.detail);
+
+/* POST NEWS */
+router.post('/', authenticated, authAdmin, validate(newValidator), newController.store);
+
+/* PUT NEWS */
+router.put('/:id', authenticated, authAdmin, newController.update);
+
+/* DELETE NEWS */
+router.delete('/:id', authenticated, authAdmin, newController.delete);
 
 module.exports = router;
