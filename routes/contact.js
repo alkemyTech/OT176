80471cf48d4b2ec569571e612d34d8d6414d111f
Nicolas D. Enrique <nicolas.enrique56@gmail.com>
2022-04-29 08:33:35 -0300

    middleware route fix

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController')
-const authAdmin = require('../middlewares/authOwnership')
+const authAdmin = require('../middlewares/authAdmin')
 
 /* GET */
 router.get("/", authAdmin, contactController.list);
