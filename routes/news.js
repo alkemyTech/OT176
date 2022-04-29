3a8a9c62a4a1a59c5448392145ce5ba85aa13a0f
maxiconstancio <maxiconstancio@gmail.com>
2022-04-29 18:10:26 -0300

    add put update news

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
 const router = express.Router();
-const newController = require('../controllers/newController');
+const validate = require('../middlewares/validate');
+const newValidator = require('../validations/news');
+const { list, store, detail, update } = require('../controllers/newController');
+const authAdmin = require('../middlewares/authAdmin');
+const authenticated = require('../middlewares/authenticated');
 
 /* GET */
-router.get('/', newController.list);
+router.get('/', list);
+router.get('/:id', authenticated, authAdmin, detail);
+
+/* POST NEWS */
+router.post('/', authenticated, authAdmin, validate(newValidator), store);
+
+/* PUT NEWS */
+router.put('/:id', authenticated, authAdmin, update);
 
 module.exports = router;
