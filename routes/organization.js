8bae7aeef0bc2d2484640bfb5ccdb91bd02ffb5f
Dany Renaudier <dani11_21@hotmail.com>
2022-04-30 15:58:36 -0300

    organization public route configured

--- routes/organization.js
+++ routes/organization.js
@@ -1,4 +1,9 @@
 const express = require('express');
-const router = express.Router();
 
-router.get('/organization/public',[],);
+const router = express.Router();
+const orgController = require('../controllers/organizationController');
+
+router.post('/newOrg', [], orgController.create);
+router.get('/public', [], orgController.getDadta);
+
+module.exports = router;
