d194e5e3d447ef927600f20615cc887d8b0795a5
Nicolas D. Enrique <nicolas.enrique56@gmail.com>
2022-05-01 10:02:52 -0300

    routes and controller fix

--- routes/contact.js
+++ routes/contact.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController');
 const authAdmin = require('../middlewares/authAdmin');
+const authenticated = require('../middlewares/authenticated');
 
 /* GET */
-router.get('/', contactController.list);
+router.get('/', authenticated, authAdmin, contactController.list);
 /* POST */
-router.post('/', authAdmin, contactController.store);
+router.post('/', contactController.store);
 
 module.exports = router;
