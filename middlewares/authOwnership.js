a1c554fc07fcda296fe1a21041ecaff6d0870e49
Nicolas D. Enrique <nicolas.enrique56@gmail.com>
2022-05-01 22:54:54 -0300

    OT176-34-middleware fix to find user by FK

--- middlewares/authOwnership.js
+++ middlewares/authOwnership.js
@@ -1,17 +1,20 @@
-const db = require('../models/index')
+const db = require('../models');
 
-const authOwnership = (model = 'Comments')  => async (req, res, next) => {
+const authOwnership = (model = 'Comments') => async (req, res, next) => {
   const itemId = req.params.id || req.body.id;
   const { id: userId } = req.user;
+  console.log(userId)
+
   if (!itemId) return res.status(400).json({ message: 'id invalido' });
 
-	try {
-		const itemUser = await db[model].findByPk(itemId);
-		if (itemUser.user_id !== userId) res.status(403).json({ message: 'Acceso Denegado' });
-			return next();
-	} catch (error) {
-		return res.status(500).send('Server error');
-	}
+  try {
+    const itemUser = await db[model].findByPk(itemId);
+	console.log(itemUser)
+    if (itemUser.user_id !== userId) return res.status(403).json({ message: 'Acceso Denegado' });
+    return next();
+  } catch (error) {
+    return res.status(500).send('Error del Servidor');
+  }
 };
 
-module.exports = { authOwnership };
+module.exports = authOwnership;
