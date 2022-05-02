70d2dafdfce54f50900ce2a5a992d52582020751
tadeogavensky <tadeogavensky.email@gmail.com>
2022-05-02 16:33:24 -0300

    ticket 92

--- controllers/documentationController.js
+++ controllers/documentationController.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 
 const documentationController = {
     docs: (req, res) => {
-
+        //Documentación en Postman
         res.redirect('https://documenter.getpostman.com/view/18268919/UyrGCExf');
 
     }
