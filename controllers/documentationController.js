3e876bb7d8ca124bab5033a2a86c2fabc45bec9d
Nicolás Darío Enrique <56316292+nicolasenrique@users.noreply.github.com>
2022-05-03 11:13:13 -0300

    merging issues fix (#98)

--- controllers/documentationController.js
+++ controllers/documentationController.js
@@ -1,12 +1,6 @@
 const fetch = require('node-fetch');
 
 const documentationController = {
-<<<<<<< HEAD
-  docs: (req, res) => {
-    res.redirect('https://documenter.getpostman.com/view/18268919/UyrGCExf');
-  },
-};
-=======
     docs: (req, res) => {
         //Documentación en Postman
         res.redirect('https://documenter.getpostman.com/view/18268919/UyrGCExf');
@@ -14,6 +8,5 @@ const documentationController = {
     }
 }
 
->>>>>>> 342e8bf3eeefadf0e23117ac0f69273a2c56b1e1
 
 module.exports = documentationController;
