ff743c9a28090a077e3ab394c36fcb435497383c
Mark <markosmk@me.com>
2022-05-04 09:30:33 -0300

    OT176-59 endpoint creation slides (#100)
    * update: add create controller and route
    * add: function to upload aws
    * update: adde filter images and change name field

--- controllers/slideController.js
+++ controllers/slideController.js
@@ -1,9 +1,28 @@
 const { Slides } = require('../models');
+const awsUpload = require('../utils/awsUpload');
 
 const getSlides = async (req, res, next) => {
   try {
     const slides = await Slides.findAll({ order: [['order', 'ASC']] });
     res.json({ slides });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createSlide = async (req, res, next) => {
+  const { text, organizationId } = req.body;
+  let { order } = req.body;
+  try {
+    if (typeof req.file === 'undefined') throw new Error('Image is required');
+    if (!order) {
+      order = await Slides.max('order') + 1;
+    }
+    const imageUrl = await awsUpload(req.file);
+    const slide = await Slides.create({
+      imageUrl, text, order, organizationId,
+    });
+    res.status(201).json({ slide });
   } catch (error) {
     next(error);
   }
@@ -23,6 +42,5 @@ const getOneSlides = async (req, res, next) => {
 };
 
 module.exports = {
-  getSlides,
-  getOneSlides,
+  getSlides, createSlide, getOneSlides,
 };
--- routes/slides.js
+++ routes/slides.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getSlides, getOneSlides } = require('../controllers/slideController');
+const { getSlides, createSlide, getOneSlides } = require('../controllers/slideController');
 const authenticated = require('../middlewares/authenticated');
 const authAdmin = require('../middlewares/authAdmin');
+const upload = require('../utils/multer');
 
 const router = express.Router();
 
 router.get('/', authenticated, authAdmin, getSlides);
 router.get('/:id', authenticated, authAdmin, getOneSlides);
+router.post('/', authenticated, authAdmin, upload('imageUrl'), createSlide);
 
 module.exports = router;
