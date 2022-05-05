f35bbaf9b6475a714945d6c3ed29d6991007f644
Mark <markosmk@me.com>
2022-05-05 11:01:53 -0300

    OT176-60 endpoint update slide (#105)
    * update: add update slide with image and endpoint
    * update: add unified helper for aws

--- routes/slides.js
+++ routes/slides.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { getSlides, createSlide, deleteSlide, getOneSlides } = require('../controllers/slideController');
+const {
+  getSlides, createSlide, deleteSlide, getOneSlides, updateSlide,
+} = require('../controllers/slideController');
 const authenticated = require('../middlewares/authenticated');
 const authAdmin = require('../middlewares/authAdmin');
 const upload = require('../utils/multer');
@@ -9,6 +11,7 @@ const router = express.Router();
 router.get('/', authenticated, authAdmin, getSlides);
 router.get('/:id', authenticated, authAdmin, getOneSlides);
 router.post('/', authenticated, authAdmin, upload('imageUrl'), createSlide);
+router.put('/:id', authenticated, authAdmin, upload('imageUrl'), updateSlide);
 router.delete('/:id', authenticated, authAdmin, deleteSlide);
 
 module.exports = router;
